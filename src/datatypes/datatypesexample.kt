a3f836c44d21846b172314bf9c0594a558b9ad53
Chinmay <ichinmay08@gmail.com>
2019-04-14 08:54:05 +0530

    Basics | Data types | Size and Default Value

---
+++ src/datatypes/datatypesexample.kt
@@ -0,0 +1,16 @@
+package datatypes
+
+//Data types in Kotlin: All are objects
+/*
+    Java ---    Kotlin      Size        Default Value       Examples
+
+    int         Int         32 bit      All variables       2, 56, -32
+    boolean -   Boolean      1 bit          must            true, false
+    byte        Byte         8 bit      be initialized      -127 to 128
+    char        Char        16 bit          in Kotlin       'a', '\n', '\101'
+    long        Long        64 bit                          -2L, 0L. 32L
+    float       Float       32 bit                          3.4f, -4.5f
+    double      Double      64 bit                          3.7D, 7.9d
+                Short       16 bit                          (none)
+
+ */
