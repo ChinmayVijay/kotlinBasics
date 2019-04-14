0f79fa12b61ddffb99ad85315b7b1b42181579f7
Chinmay <ichinmay08@gmail.com>
2019-04-14 11:54:59 +0530

    Basics | Data types |var vs val | Extended

---
+++ src/datatypes/varvalcompare.kt
@@ -0,0 +1,17 @@
+package datatypes
+
+fun main(args:Array<String>){
+    var name:String
+    name= "Doe"
+    println(name)
+
+    var age:Int= 17
+    var isAlive:Boolean = true
+    var marks:Float = 78.4F
+    var percentage= 86.5
+    var gender:Char = 'M'
+
+    val yearOfJoining:Int = 1999
+
+
+}
