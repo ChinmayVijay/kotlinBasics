fd2aafa23ac4c8e1fc501829bfbdbe1425c2cc29
Chinmay <ichinmay08@gmail.com>
2019-04-14 08:30:17 +0530

    Basics | Class | Package | Import | String Interpolation

--- src/ClassCreationExample.kt
+++ src/ClassCreationExample.kt
@@ -1,3 +1,5 @@
+import com.chinmay.User
+
 fun main(args:Array<String>){
     var user1 = User()
     user1.name="Alice"
@@ -5,18 +7,15 @@ fun main(args:Array<String>){
 
     var employee = Employee("JJ")
     employee.display()
+
+    //String interpolation
+    println("The name of the user is ${user1.name}")
+
 }
 
-class User{
 
-    var name:String=""
-    fun display(){
-        println(name)
-    }
-}
 
-class Employee(name:String){
-    var name:String=name
+class Employee(var name: String){
     fun display(){
         println(this.name)
     }
---
+++ src/com/chinmay/User.kt
@@ -0,0 +1,9 @@
+package com.chinmay
+
+class User{
+
+    var name:String=""
+    fun display(){
+        println(name)
+    }
+}
