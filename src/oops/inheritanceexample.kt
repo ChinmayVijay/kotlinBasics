1171b219270cb1097c72ba4faf7cd3c7211532a8
Chinmay <ichinmay08@gmail.com>
2019-04-23 16:28:10 +0530

    OOP Concepts | Method and Variable Overriding

--- src/oops/inheritanceexample.kt
+++ src/oops/inheritanceexample.kt
@@ -1,28 +1,42 @@
 package oops
 
 fun main(args:Array<String>){
-    var dogObj = Dog()
-    dogObj.color="white"
-    dogObj.breed="Dalmation"
-    println("The dog is ${dogObj.color} and ${dogObj.breed}")
-    dogObj.eat()
-    dogObj.bark()
+//    var dogObj = Dog()
+//    dogObj.color="white"
+//    dogObj.breed="Dalmation"
+//    println("The dog is ${dogObj.color} and ${dogObj.breed}")
+//    dogObj.eat()
+//    dogObj.bark()
+
+    var dog2 = Dog()
+    dog2.eat()
+    println(dog2.color)
+
+    var cat2 = Cat()
+    cat2.eat()
+
+    var anim = Animal()
+    anim.eat()
 }
 
 
 open class Animal{
-    var color:String=""
+    open var color:String="White"
 
-    fun eat(){
-        println("Eat")
+    open fun eat(){
+        println("Animal Eating")
     }
 }
 
 
 class Dog:Animal(){
+    override var color:String = "Black"
     var breed:String=""
     fun bark(){
         println("Woof!!")
+    }
+    override fun eat(){
+        println("Dog is eating")
     }
 }
 
@@ -30,5 +44,8 @@ class Cat:Animal(){
     var age:Int = -1
     fun meow(){
         println("Meow..!")
+    }
+    override fun eat(){
+        println("Cat is eating")
     }
 }
